d2ed48a963b7c523128d66c4fa68c3d5c1adef8d
Majd Syoufi <majd.syoufi@gmx.de>
2024-03-31 14:09:40 +0200

    added the functionality for the delete button and improved the previous code by creating seperate Fn()

--- src/app/services/calculate.service.ts
+++ src/app/services/calculate.service.ts
@@ -11,7 +11,6 @@ export class CalculateService {
   endCurrentCal: boolean = false;
 
   calculte(value: string, inputType: string) {
-
     if (inputType === 'num') {
       if (this.endCurrentCal) {
         this.firstNum = '';
@@ -21,37 +20,46 @@ export class CalculateService {
         this.endCurrentCal = false;
       }
       this.firstNum += value;
-      // console.log('first number ' + this.firstNum);
-      // console.log('second number ' + this.secondNum);
+      console.log('first number ' + this.firstNum);
+      console.log('second number ' + this.secondNum);
     } else {
       if (this.firstNum === '') return;
 
       if (inputType === 'oper') {
-        if (this.secondNum !== '') {
+        this.calculateOperation(value);
 
-          this.secondNum = this.calculateResult().toString();
-          this.firstNum = '';
-          this.operator = value;
-          this.endCurrentCal = false;
-          // console.log('first number ' + this.firstNum);
-          // console.log('second number ' + this.secondNum);
-          // console.log('result ' + this.result);
-          // console.log('operator ' + this.operator);
-          return
-        }
-
-        this.secondNum = this.firstNum;
-        this.firstNum = '';
-        this.operator = value;
-
-        // console.log('first number ' + this.firstNum);
-        // console.log('second number ' + this.secondNum);
-        // console.log('operator ' + this.operator);
       } else if (inputType === 'equal') {
         if (this.secondNum === '') return;
         this.calculateResult();
+
+      } else if (inputType === 'delete') {
+        this.deleteLastInput();
+
       }
     }
+  }
+
+  calculateOperation(value: string) {
+    if (this.secondNum !== '') {
+
+      this.secondNum = this.calculateResult().toString();
+      this.firstNum = '';
+      this.operator = value;
+      this.endCurrentCal = false;
+      console.log('first number ' + this.firstNum);
+      console.log('second number ' + this.secondNum);
+      console.log('result ' + this.result);
+      console.log('operator ' + this.operator);
+      return
+    }
+
+    this.secondNum = this.firstNum;
+    this.firstNum = '';
+    this.operator = value;
+
+    console.log('first number ' + this.firstNum);
+    console.log('second number ' + this.secondNum);
+    console.log('operator ' + this.operator);
   }
 
   calculateResult() {
@@ -70,13 +78,23 @@ export class CalculateService {
     }
     this.result = result;
     this.endCurrentCal = true;
-    // console.log('first number ' + this.firstNum);
-    // console.log('second number ' + this.secondNum);
-    // console.log('result ' + this.result);
+    console.log('first number ' + this.firstNum);
+    console.log('second number ' + this.secondNum);
+    console.log('result ' + this.result);
     return result;
   }
 
-  renderInputAndResult() {
-
+  deleteLastInput() {
+    if (this.endCurrentCal) {
+      this.firstNum = this.result.toString();
+      this.secondNum = '';
+      this.operator = '';
+      this.result = 0;
+      this.endCurrentCal = false;
+    }
+    this.firstNum =
+      this.firstNum.slice(0, this.firstNum.length - 1);
+    console.log('first number ' + this.firstNum);
+    console.log('result ' + this.result);
   }
 }
