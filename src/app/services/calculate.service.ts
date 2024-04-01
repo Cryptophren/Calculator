38ba29911d36bcdbd192ada6401bc1d4e7771631
Majd Syoufi <majd.syoufi@gmx.de>
2024-04-01 11:47:54 +0200

    bug fix and cleaning the code. Preparing for dispaly rendering of input and results

--- src/app/services/calculate.service.ts
+++ src/app/services/calculate.service.ts
@@ -10,52 +10,75 @@ export class CalculateService {
   result: number = 0;
   endCurrentCal: boolean = false;
 
+  upperDisplay: string = '';
+  lowerDisplay: string = '';
+
+  // a counter just for the console for debugging
+  i = 1;
+
   calculte(value: string, inputType: string) {
     if (inputType === 'num' && this.firstNum.length < 15) {
+      if (this.endCurrentCal) this.resetCalculator();
 
-      if (value === "0" && this.firstNum === '0') return;
+      if (this.firstNum === '0' && value === "0") return;
 
-      if (this.endCurrentCal) {
-        this.resetCalculator();
+      if (this.firstNum === '0' && value !== "0") {
+        this.firstNum = value;
+        return
       }
+
       this.firstNum += value;
 
     } else {
-      if (this.firstNum === '' && value === ',') this.firstNum = '0.';
+      // Exeptions filter for the first input
+      if (this.firstNum === ''
+        && inputType !== 'clear'
+        && inputType !== 'delete'
+        && inputType !== 'comma'
+      ) return;
 
-      if (this.firstNum === '') return;
-
-      if (inputType === 'oper') {
-        this.calculateOperation(value);
-
-      } else if (inputType === 'percent') {
-        this.firstNum = (+this.firstNum / 100).toString();
-
-      } else if (inputType === 'delete') {
-        this.continueWithResult();
-        this.firstNum = this.firstNum.slice(0, this.firstNum.length - 1);
-
-      } else if (inputType === 'clear') {
-        this.resetCalculator();
-
-      } else if (inputType === 'convert') {
-        this.continueWithResult();
-        this.firstNum = (-+this.firstNum).toString();
-
-      } else if (inputType === 'comma') {
-        const commaFound = this.firstNum.includes('.');
-        if (!commaFound) this.firstNum += '.';
-
-      } else if (inputType === 'equal') {
-        if (this.secondNum !== '')
-          this.calculateResult();
+      switch (inputType) {
+        case 'oper':
+          this.calculateOperation(value);
+          break;
+        case 'percent':
+          this.firstNum = (+this.firstNum / 100).toString();
+          break;
+        case 'comma':
+          if (this.firstNum === '') this.firstNum = '0.';
+          if (!this.firstNum.includes('.')) this.firstNum += '.';
+          break;
+        case 'convert':
+          this.continueWithResult();
+          this.firstNum = (-+this.firstNum).toString();
+          break;
+        case 'equal':
+          if (this.secondNum !== '') this.calculateResult();
+          break;
+        case 'delete':
+          this.continueWithResult();
+          this.firstNum = this.firstNum.slice(0, this.firstNum.length - 1);
+          break;
+        case 'clear':
+          this.resetCalculator();
+          break;
       }
     }
+    console.log(this.i++);
+    console.log('firstNum :' + this.firstNum);
+    console.log('operator :' + this.operator);
+    console.log('secondeNum :' + this.secondNum);
+    console.log('result :' + this.result);
+    console.log('endCurrentCal :' + this.endCurrentCal);
+    console.log('-------------------------');
   }
 
   calculateOperation(value: string) {
     if (this.secondNum !== '') {
+
       this.secondNum = this.calculateResult().toString();
+      if (this.secondNum === '') return
+
       this.firstNum = '';
       this.operator = value;
       this.endCurrentCal = false;
@@ -73,18 +96,18 @@ export class CalculateService {
     if (this.operator === '/') {
       if (this.firstNum === '0') {
         alert('Division by zero is undefined!');
-        this.firstNum = '';
+        this.resetCalculator();
         return '';
       }
       result = +this.secondNum / +this.firstNum;
 
-    } else if (this.operator === '+') {
-      result = +this.firstNum + +this.secondNum;
+    } else if (this.operator === '*') {
+      result = +this.firstNum * +this.secondNum;
 
     } else if (this.operator === '-') {
       result = +this.secondNum - +this.firstNum;
 
-    } else result = +this.firstNum * +this.secondNum;
+    } else result = +this.firstNum + +this.secondNum;
 
     this.result = result;
     this.endCurrentCal = true;
@@ -108,4 +131,41 @@ export class CalculateService {
     this.result = 0;
     this.endCurrentCal = false;
   }
+
+  /*
+    renderInput(value: string, inputType: string) {
+      if (inputType === 'num') {
+        this.upperDisplay += value;
+  
+      } else {
+        if (inputType === 'equal' && this.result === 0) return;
+  
+        switch (inputType) {
+          case 'oper':
+            this.upperDisplay += value;
+            break;
+          case 'percent':
+  
+            break;
+          case 'delete':
+            this.upperDisplay = this.upperDisplay.slice(0, this.upperDisplay.length - 1);
+            this.lowerDisplay = '';
+            break;
+          case 'clear':
+            this.upperDisplay = '';
+            this.lowerDisplay = '';
+            break;
+          case 'convert':
+  
+            break;
+          case 'comma':
+            this.upperDisplay += value;
+            break;
+          case 'equal':
+            this.lowerDisplay = this.result.toString();
+            break;
+        }
+      }
+    }  */
 }
+
